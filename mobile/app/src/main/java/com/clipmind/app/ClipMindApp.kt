e7de9c40d878c6c829f3c0d62e17728625ff18f2
jay117king <138710811+jay117king@users.noreply.github.com>
2026-08-07 21:40:16 +0200

    Add Application class

---
+++ mobile/app/src/main/java/com/clipmind/app/ClipMindApp.kt
@@ -0,0 +1,10 @@
+package com.clipmind.app
+
+import android.app.Application
+
+class ClipMindApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // TODO: Initialize Room database, WorkManager, etc.
+    }
+}
